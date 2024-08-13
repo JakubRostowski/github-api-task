9332d1fdcfeb00d56986c0278f62dadc62b3180a
JakubRostowski <rostowskijakub@gmail.com>
2024-08-13 19:10:43 +0200

    init

---
+++ src/test/java/pl/jakubrostowski/githubapitask/GithubApiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jakubrostowski.githubapitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApiTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
